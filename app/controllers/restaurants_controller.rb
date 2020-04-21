2edcf429129340daa901fd12227d3acde661aa2d
Felipe Americano <felip.americano@gmail.com>
2020-04-21 20:42:36 -0300

    rake verde bebeee, era só colocar uma array no lugar do %w que validou as integers

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,50 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_Restaurant, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @restaurants = Restaurant.all
+    end
+
+    def show
+    end
+
+    def new
+      @restaurant = Restaurant.new
+    end
+
+    def destroy
+      @restaurant.destroy
+
+      # no need for app/views/restaurants/destroy.html.erb
+      redirect_to restaurants_path
+    end
+
+    def create
+      @restaurant = Restaurant.new(restaurant_params)
+      @restaurant.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+      redirect_to restaurant_path(@restaurant)
+    end
+
+    def edit
+    end
+
+    def update
+      @restaurant.update(restaurant_params)
+
+      redirect_to restaurant_path(@restaurant)
+    end
+
+    private
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:title, :content)
+    end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Lorem.paragraph_by_chars(number: 50, supplemental: false),
+    phone_number: Faker::Number.number(digits: 8),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+puts "Finished!"
